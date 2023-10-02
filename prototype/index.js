7a4ebd417a276e1ca3d17174560eea200b8807d2
Abrarul Hasan <abrarulhasan600@gmail.com>
2023-10-03 00:57:46 +0600

    new add

--- prototype/index.js
+++ prototype/index.js
@@ -6,3 +6,17 @@ const myObject = {
   };
   
   myObject.greet(); // Greetings from Dhaka
+
+
+//Shadowing properties
+
+const myDate = new Date(1995, 11, 17);
+
+console.log(myDate.getYear()); // 95
+
+myDate.getYear = function () {
+  console.log("something else!");
+};
+
+myDate.getYear();
+
