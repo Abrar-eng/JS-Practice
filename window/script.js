7e1294a0d1c52284f88209a903bdc40526e7b893
Abrarul Hasan <abrarulhasan600@gmail.com>
2023-10-04 20:58:25 +0600

    new

---
+++ window/script.js
@@ -0,0 +1,21 @@
+//Alert a Message
+
+window.alert("Hello, World!");
+
+// Open a New Window
+
+window.open("https://www.facebook.com");
+
+//Manipulate Location
+
+window.location.href = "https://www.twitter.com";
+
+//Set Timeout
+
+window.setTimeout(function() {
+    window.alert("Delayed alert!");
+}, 1000);
+
+
+
+
